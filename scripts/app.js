4e1021badd18d36a72bdb463b958169fb8b3de11
AndreaRguez <aerna2124@gmail.com>
2018-12-04 12:27:13 +0100

    Logo, search bar and map added Problem witch open/close menu solved

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,8 @@
-function openSlideMenu(){
-            document.getElementById("menu").style.width = "250px";
-            document.getElementById("content").style.marginLeft = "250px";
-            
-        }
-        function closeSlideMenu(){
-            document.getElementById("menu").style.width = "0px";
-            document.getElementById("content").style.marginLeft = "0px";
-        }
+function openSlideMenu() {
+    document.getElementById("menu").style.width = "250px";
 
-        function openClose(){
-            var open =  document.getElementById("menu").style.width == "250px";
-            if(open){
-                closeSlideMenu();
-            }else{
-                openSlideMenu();
-            }
-        }
+}
+function closeSlideMenu() {
+	document.getElementById("menu").style.width = "0px";
+    document.getElementById("content").style.marginLeft = "0px";
+}
