edd06b72dfb4f58de4a3a60e838f5cedbef68137
Skyggfisk <mahasi93@hotmail.com>
2018-12-12 23:38:36 +0100

    Add eventListener for fetch event. General clean up + formatting

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 //start install
-self.addEventListener('install', function (event) {
-
-    event.waitUntil(
-        caches.open('astc_cache')
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll([
-                    'index.html',
-                    './views/astc-map.html',
-                    './styles/style.css',
-                    './styles/mapStyle.css',
-                    './scripts/app.js',
-                    './images/favicon.png',
-                    './images/aalborgstor.png'
-                ]);
-                console.log('Caching done');
-            })
-    );
-    
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("astc_cache").then(cache => {
+      console.log("[ServiceWorker] Opened cache");
+      return cache.addAll([
+        "index.html",
+        "./views/astc-map.html",
+        "./styles/style.css",
+        "./styles/mapStyle.css",
+        "./scripts/app.js",
+        "./images/favicon.png",
+        "./images/aalborgstor.png"
+      ]);
+    })
+  );
 });
 
-//self.addEventListener('fetch')
+self.addEventListener("fetch", e => {
+  e.respondWith(async response => {
+    console.log(response);
+  });
+});
