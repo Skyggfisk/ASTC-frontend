622efd20171494c92bc7046675d1dd7ce8798f97
Nándor Rácz <1062051@ucn.dk>
2018-12-12 14:34:59 +0100

    sw test 8

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function (event) {
         caches.open('astc_cache')
             .then(function (cache) {
                 console.log('Opened cache');
-                return cache.addAll(
+                return cache.addAll([
                     'index.html',
                     './views/astc-map.html',
                     './styles/style.css',
@@ -13,7 +13,7 @@ self.addEventListener('install', function (event) {
                     './scripts/app.js',
                     './images/favicon.png',
                     './images/aalborgstor.png'
-                );
+                ]);
                 console.log('Caching done');
             })
     );
