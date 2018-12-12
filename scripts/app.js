9ca8486e2c3ab3b3d11548fbbf29d1ff331c7adc
Nándor Rácz <1062051@ucn.dk>
2018-12-12 14:09:46 +0100

    sw test2

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 //Service worker registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('../service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
