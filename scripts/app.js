bc185764f096a3e26c521f8441bb2fce81181ae5
Nándor Rácz <1062051@ucn.dk>
2018-12-12 14:28:11 +0100

    sw test 6

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 //Service worker registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/scripts/service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('ASTC-frontend/scripts/service-worker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
