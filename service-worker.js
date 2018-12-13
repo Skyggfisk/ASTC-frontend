457040180a4da9c7a98a178df080bf5f33edcec7
Skyggfisk <mahasi93@hotmail.com>
2018-12-13 12:32:30 +0100

    Remove unused console log. Add formatting

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,37 @@
 //start install
-self.addEventListener('install', function (event) {
-
-    event.waitUntil(
-        caches.open('astc_cache')
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll([
-                    'index.html',
-                    './views/astc-map.html',
-                    './styles/style.css',
-                    './styles/mapStyle.css',
-                    './scripts/app.js',
-                    './images/favicon.png',
-                    './images/aalborgstor.png'
-                ]);
-                console.log('Caching done');
-            })
-    );
-    
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("astc_cache").then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll([
+        "index.html",
+        "./views/astc-map.html",
+        "./styles/style.css",
+        "./styles/mapStyle.css",
+        "./scripts/app.js",
+        "./images/favicon.png",
+        "./images/aalborgstor.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.match(event.request).then(function (response) {
-        if (response !== undefined) {
-            return response;
-        } else {
-            return fetch(event.request).then(function (response) {
-                let responseClone = response.clone();
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function(response) {
+          let responseClone = response.clone();
 
-                caches.open('astc_cache').then(function (cache) {
-                    cache.put(event.request, responseClone);
-                });
-                console.log(response);
-                return response;
-            })
-        }
-    }));
+          caches.open("astc_cache").then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+          console.log(response);
+          return response;
+        });
+      }
+    })
+  );
 });
