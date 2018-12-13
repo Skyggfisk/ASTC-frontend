f9afd6581dd75a27ea2cd436443366508e22e06e
Skyggfisk <mahasi93@hotmail.com>
2018-12-13 12:34:01 +0100

    Add formatting. Change console output for debugging.

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,20 @@
 //Service worker registration
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("./service-worker.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
 }
 
 // open the drawer menu from the left
@@ -23,65 +29,62 @@ function closeSlideMenu() {
 
 //Display only the home page and search bar
 function loadHome() {
+  var homecontent = document.getElementById("content-home");
+  homecontent.hidden = false;
 
-    var homecontent = document.getElementById("content-home");
-    homecontent.hidden = false;
+  var mapcontent = document.getElementById("content-map");
+  mapcontent.hidden = true;
 
-    var mapcontent = document.getElementById("content-map");
-    mapcontent.hidden = true;
+  var aboutcontent = document.getElementById("content-about");
+  aboutcontent.hidden = true;
 
-    var aboutcontent = document.getElementById("content-about");
-    aboutcontent.hidden = true;
+  var searchbar = document.getElementById("search-bar");
+  searchbar.hidden = false;
 
-    var searchbar = document.getElementById("search-bar");
-    searchbar.hidden = false;
+  var searchcontent = document.getElementById("content-searchresults");
+  searchcontent.hidden = true;
 
-    var searchcontent = document.getElementById("content-searchresults");
-    searchcontent.hidden = true;
-
-    closeSlideMenu();
+  closeSlideMenu();
 }
 
 //Display only the "map" page and search bar
 function loadMap() {
+  var homecontent = document.getElementById("content-home");
+  homecontent.hidden = true;
 
-    var homecontent = document.getElementById("content-home");
-    homecontent.hidden = true;
+  var mapcontent = document.getElementById("content-map");
+  mapcontent.hidden = false;
 
-    var mapcontent = document.getElementById("content-map");
-    mapcontent.hidden = false;
+  var aboutcontent = document.getElementById("content-about");
+  aboutcontent.hidden = true;
 
-    var aboutcontent = document.getElementById("content-about");
-    aboutcontent.hidden = true;
+  var searchbar = document.getElementById("search-bar");
+  searchbar.hidden = false;
 
-    var searchbar = document.getElementById("search-bar");
-    searchbar.hidden = false;
+  var searchcontent = document.getElementById("content-searchresults");
+  searchcontent.hidden = true;
 
-    var searchcontent = document.getElementById("content-searchresults");
-    searchcontent.hidden = true;
-
-    closeSlideMenu();
+  closeSlideMenu();
 }
 
 //Display only the "about" page
 function loadAbout() {
+  var homecontent = document.getElementById("content-home");
+  homecontent.hidden = true;
 
-    var homecontent = document.getElementById("content-home");
-    homecontent.hidden = true;
+  var mapcontent = document.getElementById("content-map");
+  mapcontent.hidden = true;
 
-    var mapcontent = document.getElementById("content-map");
-    mapcontent.hidden = true;
+  var aboutcontent = document.getElementById("content-about");
+  aboutcontent.hidden = false;
 
-    var aboutcontent = document.getElementById("content-about");
-    aboutcontent.hidden = false;
+  var searchbar = document.getElementById("search-bar");
+  searchbar.hidden = true;
 
-    var searchbar = document.getElementById("search-bar");
-    searchbar.hidden = true;
+  var searchcontent = document.getElementById("content-searchresults");
+  searchcontent.hidden = true;
 
-    var searchcontent = document.getElementById("content-searchresults");
-    searchcontent.hidden = true;
-
-    closeSlideMenu();
+  closeSlideMenu();
 }
 
 // TODO should ideally place a spinner while waiting for results to build
@@ -93,116 +96,113 @@ function loadAbout() {
 
 //The search function, currently only looking through products and not stores
 function searchHandler() {
+  //hide all content besides the search results
+  var homecontent = document.getElementById("content-home");
+  homecontent.hidden = true;
 
-    //hide all content besides the search results
-    var homecontent = document.getElementById("content-home");
-    homecontent.hidden = true;
+  var mapcontent = document.getElementById("content-map");
+  mapcontent.hidden = true;
 
-    var mapcontent = document.getElementById("content-map");
-    mapcontent.hidden = true;
+  var aboutcontent = document.getElementById("content-about");
+  aboutcontent.hidden = true;
 
-    var aboutcontent = document.getElementById("content-about");
-    aboutcontent.hidden = true;
+  var searchcontent = document.getElementById("content-searchresults");
+  searchcontent.hidden = false;
 
-    var searchcontent = document.getElementById("content-searchresults");
-    searchcontent.hidden = false;
+  //clear previous results
+  searchcontent.innerHTML = "";
 
-    //clear previous results
-    searchcontent.innerHTML = "";
+  //get search bar input
+  let searchquery = document.getElementById("search-text").value;
 
-    //get search bar input
-    let searchquery = document.getElementById("search-text").value;
+  //create url for the api
+  let searchurl = "https://astcapi.azurewebsites.net/api/search/" + searchquery;
 
-    //create url for the api
-    let searchurl = "https://astcapi.azurewebsites.net/api/search/" + searchquery;
-
-    //check if search query is empty, and display an error
-    if (searchquery == "") {
-
-        let errortext =
-            `<div class="error">
+  //check if search query is empty, and display an error
+  if (searchquery == "") {
+    let errortext = `<div class="error">
             Please enter a search term.
-        </div>`
+        </div>`;
 
-        searchcontent.innerHTML = errortext;
+    searchcontent.innerHTML = errortext;
 
-      //if search isn't empty, get data from api
-    } else {
-        fetch(searchurl)
-            .then(
-                //if http response isn't 200 ("okay"), print an error to the console
-                function (response) {
-                    if (response.status !== 200) {
-                        console.log('Looks like there was a problem. Status Code: ' +
-                            response.status);
-                        return;
-                    }
-                    //get the api returned data and save it as the array "data"
-                    response.json().then(function (data) {
-
-                        //if there are no results, show an error
-                        if (!Array.isArray(data) || !data.length) {
-
-                            let errortext =
-                            `<div class="error">
+    //if search isn't empty, get data from api
+  } else {
+    fetch(searchurl)
+      .then(
+        //if http response isn't 200 ("okay"), print an error to the console
+        function(response) {
+          if (response.status !== 200) {
+            console.log(
+              `Looks like there was a problem. Status Code: ${response.status}`
+            );
+            return;
+          }
+          //get the api returned data and save it as the array "data"
+          response.json().then(function(data) {
+            //if there are no results, show an error
+            if (!Array.isArray(data) || !data.length) {
+              let errortext = `<div class="error">
                                 No results for the product: ${searchquery}
-                            </div>`
+                            </div>`;
 
-                            searchcontent.innerHTML = errortext;
-                        }
+              searchcontent.innerHTML = errortext;
+            }
 
-                        //if there are results, create a new html element for each one
-                        for (let i = 0; i < data.length; i++) {
+            //if there are results, create a new html element for each one
+            for (let i = 0; i < data.length; i++) {
+              let productid = "productdd" + i;
 
-                            let productid = "productdd" + i;
-
-                            let product =
-                                `<div class="result">
+              let product = `<div class="result">
                                 <img
                                     class="product-image"
                                     src="https://via.placeholder.com/200?text=Product+image"
                                     alt="image missing"
                                 />
-                                <p class="product-name">${data[i].productName}</p>
+                                <p class="product-name">${
+                                  data[i].productName
+                                }</p>
                                 <a href="#" class="seller-dropdown" onclick="toggleSellers('${productid}')">
                                     <i class="fas fa-angle-left"></i>
                                 </a>
                                 <div class="seller-info seller-hidden" id="${productid}">`;
 
-                            for (let j = 0; j < data[i].productSellers.length; j++) {
+              for (let j = 0; j < data[i].productSellers.length; j++) {
+                console.log(
+                  data[i].productSellers[j].shopname.replace(/\s+/g, "")
+                );
+                // let iframe = document.getElementById("frame");
+                // let selectedTarget = iframe.contentWindow.document.getElementById(
+                //   `${data[i].productSellers[j].shopname.replace(/\s+/g, "")}`
+                // );
 
-                                console.log(data[i].productSellers[j]);
+                let seller = `<p class="seller-name">${
+                  data[i].productSellers[j].shopname
+                }</p>
+                  <p class="seller-price">${
+                    data[i].productSellers[j].price
+                  }</p></br>`;
 
-                                let seller =
-                                `<p class="seller-name">${data[i].productSellers[j].shopname}</p>
-                                <p class="seller-price">${data[i].productSellers[j].price}</p>
-                                </br>`;
+                product += seller;
+              }
 
-                                product += seller;
-                            }
+              product += `</div></div>`;
 
-                            product += `</div></div>`
-
-                            searchcontent.innerHTML += product;
-
-                        }
-                    });
-
-                }
-            )
-                .catch(function (err) {
-                    console.log('Fetch Error :-S', err);
-                });
-    }
-
-    
-
+              searchcontent.innerHTML += product;
+            }
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log("Fetch Error :-S", err);
+      });
+  }
 }
 
 function toggleSellers(id) {
-    let target = document.getElementById(id);
+  let target = document.getElementById(id);
 
-    target.classList.toggle("seller-hidden");
+  target.classList.toggle("seller-hidden");
 }
 
 // Replaces the current app with the map
@@ -214,19 +214,18 @@ function toggleSellers(id) {
 //}
 
 //function displayOnMap(product) {
-//    let 
+//    let
 //}
 
-function shop(){
+function shop() {
   console.log("start");
-    var iframe = document.getElementById("frame");
-    //console.log(iframe);
-    //console.log(iframe.contentWindow.document.getElementById("map").innerHTML);
-    var selectedtarget = iframe.contentWindow.document.getElementById("BR");
+  var iframe = document.getElementById("frame");
+  //console.log(iframe);
+  //console.log(iframe.contentWindow.document.getElementById("map").innerHTML);
+  var selectedtarget = iframe.contentWindow.document.getElementById("BR");
 
-    selectedtarget.classList.toggle("selectedshop");
-    //console.log(selectedtarget.classList);
-    //selectedtarget.classlist.add("test-2-success");
-    //console.log(selectedtarget.classList);
+  selectedtarget.classList.toggle("selectedshop");
+  //console.log(selectedtarget.classList);
+  //selectedtarget.classlist.add("test-2-success");
+  //console.log(selectedtarget.classList);
 }
-
